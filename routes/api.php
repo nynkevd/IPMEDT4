ad04e2a88866fb0591eb6063207c2d529b897266
Anouk <aananouk@gmail.com>
2019-05-30 15:57:25 +0200

    Cors middleware voor elke api route

--- routes/api.php
+++ routes/api.php
@@ -12,9 +12,9 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::middleware('auth:api')->get('/match/{user}', 'MatchingController@maakMatch');
-Route::middleware('auth:api')->get('/interests/{user}', 'InterestsController@getInterests');
-Route::middleware('auth:api')->get('/pictures', 'ProfilePicturesContoller@getProfilePictures');
+Route::middleware('auth:api')->get('/match/{user}', 'MatchingController@maakMatch');->middleware('cors')
+Route::middleware('auth:api')->get('/interests/{user}', 'InterestsController@getInterests')->middleware('cors');
+Route::middleware('auth:api')->get('/pictures', 'ProfilePicturesContoller@getProfilePictures')->middleware('cors');
 Route::middleware('auth:api')->get('/userinfo/{user}', 'UserInfoController@getUserInfo')->middleware('cors');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
