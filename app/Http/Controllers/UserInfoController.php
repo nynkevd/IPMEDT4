847a6fcced5fce11a51e305a02d14d9017180c03
nynkevd <37672196+nynkevd@users.noreply.github.com>
2019-06-06 10:12:35 +0200

    firstLogin added

--- app/Http/Controllers/UserInfoController.php
+++ app/Http/Controllers/UserInfoController.php
@@ -11,9 +11,10 @@ class UserInfoController extends Controller
 {
     public function getUserInfo($currentUser){
       $name = Accounts::where(['username' => $currentUser])->get('name');
+      $firstLogin = Accounts::where(['username'=>$currentUser])->get('firstLogin');
       $ageArr = UserInfo::where(['username' => $currentUser])->get('age');
-      $fromArr = UserInfo::where(['username' => $currentUser])->get('from');
-      $toArr = UserInfo::where(['username' => $currentUser])->get('to');
+      $fromArr = UserInfo::where(['username' => $currentUser])->get('travelFrom');
+      $toArr = UserInfo::where(['username' => $currentUser])->get('travelTo');
       $picId = UserInfo::where(['username' => $currentUser])->get('profile_picture');
       $picId = $picId{0};
       $pic = $picId{'profile_picture'};
@@ -24,23 +25,25 @@ class UserInfoController extends Controller
 
       $name = $name{0};
       $name = $name{'name'};
+      $firstLogin = $firstLogin{0};
+      $firstLogin = $firstLogin{'firstLogin'};
       $ageArr = $ageArr{0};
       $age = $ageArr{'age'};
       $fromArr = $fromArr{0};
-      $from = $fromArr{'from'};
+      $from = $fromArr{'travelFrom'};
       $toArr = $toArr{0};
-      $to = $toArr{'to'};
+      $to = $toArr{'travelTo'};
 
       $userInfo = [
         "name" => $name,
         "age" => $age,
         "from" => $from,
         "to" => $to,
-        "picture" => $picLink
+        "picture" => $picLink,
+        "firstLogin" => $firstLogin,
       ];
 
       return $userInfo;
     }
-
 
 }
