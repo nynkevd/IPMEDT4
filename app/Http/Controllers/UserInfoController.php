fe519c4a19483e2b60466e9829db262b6cee0003
nynkevd <37672196+nynkevd@users.noreply.github.com>
2019-06-06 11:21:03 +0200

    Revert "firstLogin added"
    This reverts commit 847a6fcced5fce11a51e305a02d14d9017180c03.

--- app/Http/Controllers/UserInfoController.php
+++ app/Http/Controllers/UserInfoController.php
@@ -11,10 +11,9 @@ class UserInfoController extends Controller
 {
     public function getUserInfo($currentUser){
       $name = Accounts::where(['username' => $currentUser])->get('name');
-      $firstLogin = Accounts::where(['username'=>$currentUser])->get('firstLogin');
       $ageArr = UserInfo::where(['username' => $currentUser])->get('age');
-      $fromArr = UserInfo::where(['username' => $currentUser])->get('travelFrom');
-      $toArr = UserInfo::where(['username' => $currentUser])->get('travelTo');
+      $fromArr = UserInfo::where(['username' => $currentUser])->get('from');
+      $toArr = UserInfo::where(['username' => $currentUser])->get('to');
       $picId = UserInfo::where(['username' => $currentUser])->get('profile_picture');
       $picId = $picId{0};
       $pic = $picId{'profile_picture'};
@@ -25,25 +24,23 @@ class UserInfoController extends Controller
 
       $name = $name{0};
       $name = $name{'name'};
-      $firstLogin = $firstLogin{0};
-      $firstLogin = $firstLogin{'firstLogin'};
       $ageArr = $ageArr{0};
       $age = $ageArr{'age'};
       $fromArr = $fromArr{0};
-      $from = $fromArr{'travelFrom'};
+      $from = $fromArr{'from'};
       $toArr = $toArr{0};
-      $to = $toArr{'travelTo'};
+      $to = $toArr{'to'};
 
       $userInfo = [
         "name" => $name,
         "age" => $age,
         "from" => $from,
         "to" => $to,
-        "picture" => $picLink,
-        "firstLogin" => $firstLogin,
+        "picture" => $picLink
       ];
 
       return $userInfo;
     }
+
 
 }
