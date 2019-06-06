dd9c8e1fddc49b043e1174b9990133bb8e8fe0dc
nynkevd <37672196+nynkevd@users.noreply.github.com>
2019-06-06 11:22:02 +0200

    Update UserInfoController.php

--- app/Http/Controllers/UserInfoController.php
+++ app/Http/Controllers/UserInfoController.php
@@ -12,8 +12,8 @@ class UserInfoController extends Controller
     public function getUserInfo($currentUser){
       $name = Accounts::where(['username' => $currentUser])->get('name');
       $ageArr = UserInfo::where(['username' => $currentUser])->get('age');
-      $fromArr = UserInfo::where(['username' => $currentUser])->get('from');
-      $toArr = UserInfo::where(['username' => $currentUser])->get('to');
+      $fromArr = UserInfo::where(['username' => $currentUser])->get('travelFrom');
+      $toArr = UserInfo::where(['username' => $currentUser])->get('travelTo');
       $picId = UserInfo::where(['username' => $currentUser])->get('profile_picture');
       $picId = $picId{0};
       $pic = $picId{'profile_picture'};
@@ -27,9 +27,9 @@ class UserInfoController extends Controller
       $ageArr = $ageArr{0};
       $age = $ageArr{'age'};
       $fromArr = $fromArr{0};
-      $from = $fromArr{'from'};
+      $from = $fromArr{'travelFrom'};
       $toArr = $toArr{0};
-      $to = $toArr{'to'};
+      $to = $toArr{'travelTo'};
 
       $userInfo = [
         "name" => $name,
