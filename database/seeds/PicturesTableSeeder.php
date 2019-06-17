76982c170a332226e520b67dc9d79adf859dd523
Anouk <aananouk@gmail.com>
2019-06-17 13:56:33 +0200

    Foto's 'lokaal' opgeslagen + nieuwe url's

--- database/seeds/PicturesTableSeeder.php
+++ database/seeds/PicturesTableSeeder.php
@@ -11,19 +11,49 @@ class PicturesTableSeeder extends Seeder
      */
     public function run()
     {
+      // DB::table('pictures')->insert([
+      //   'id'=> 1,
+      //   'link' => "/img/aap.jpeg"
+      // ]);
+      //
+      // DB::table('pictures')->insert([
+      //   'id'=> 2,
+      //   'link' => "."
+      // ]);
+      //
+      // DB::table('pictures')->insert([
+      //   'id'=> 3,
+      //   'link' => "/img/beest.png"
+      // ]);
+      //
+      // DB::table('pictures')->insert([
+      //   'id'=> 4,
+      //   'link' => "/img/geitje.png"
+      // ]);
+
       DB::table('pictures')->insert([
-        'id'=> 1,
-        'link' => "https://upload.wikimedia.org/wikipedia/commons/2/22/Pictor_A_composite.jpg"
+        'id'=> 5,
+        'link' => "/img/giraffe.png"
       ]);
 
       DB::table('pictures')->insert([
-        'id'=> 2,
-        'link' => "https://purperme.files.wordpress.com/2018/04/image-1-1.jpg?w=336&h=336&crop=1"
+        'id'=> 6,
+        'link' => "/img/hond.png"
       ]);
 
       DB::table('pictures')->insert([
-        'id'=> 3,
-        'link' => "https://is4-ssl.mzstatic.com/image/thumb/Purple128/v4/54/4f/9b/544f9b86-5073-2793-f825-699c9f547375/AppIcon-1x_U007emarketing-0-85-220-0-5.png/246x0w.jpg"
+        'id'=> 7,
+        'link' => "/img/kat.png"
+      ]);
+
+      DB::table('pictures')->insert([
+        'id'=> 8,
+        'link' => "/img/kleinBeest.png"
+      ]);
+
+      DB::table('pictures')->insert([
+        'id'=> 9,
+        'link' => "/img/konijn.png"
       ]);
 
     }
